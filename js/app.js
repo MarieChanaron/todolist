05b7032622bcea807cac1d0133dc9613cbbdb147
Marie Chanaron <chanaronmarie@gmail.com>
2023-04-03 10:17:14 +0200

    add the readme file

--- js/app.js
+++ js/app.js
@@ -1,42 +1,57 @@
 import { generateRandomTodos } from "./generateRandomTodos.js";
 
-const task = generateRandomTodos();
+
 
 // const state = task.done;
 // const id = task.id;
 // const description = task.text;
-// const user = {
-//     name: task.user.name, 
+// const taskUser = {
+//     name: task.user.name,
 //     icon: task.user.icon
 // }
 
-const tableBody = document.querySelector('tbody');
+const addTaskToTable = (task) => {
+  const tableBody = document.querySelector("tbody");
 
-const row = document.createElement('tr');
+  const row = document.createElement("tr");
+  row.setAttribute("id", task.id);
 
-const firstColumn = document.createElement('td');
-const checkbox = document.createElement('input');
-checkbox.setAttribute('type', 'checkbox');
-// if (state) {
-//     checkbox.setAttribute('checked', true);
-// } 
-firstColumn.appendChild(checkbox);
-row.appendChild(firstColumn);
+  const firstColumn = document.createElement("td");
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  if (task.done === true) {
+    checkbox.setAttribute("checked", true);
+  }
+  firstColumn.appendChild(checkbox);
+  row.appendChild(firstColumn);
 
-const secondColumn = document.createElement('td');
-const taskDescription = document.createElement('p');
-secondColumn.appendChild(taskDescription);
-row.appendChild(secondColumn);
+  const secondColumn = document.createElement("td");
+  const taskDescription = document.createElement("p");
+  secondColumn.appendChild(taskDescription);
+  taskDescription.innerText = task.text;
+  row.appendChild(secondColumn);
 
-const thirdColumn = document.createElement('td');
-const user = document.createElement('p');
-const userName = document.createElement('span');
-const userIcon = document.createElement('span');
-user.appendChild(userName);
-user.appendChild(userIcon);
-thirdColumn.appendChild(user);
-row.appendChild(thirdColumn);
+  const thirdColumn = document.createElement("td");
+  const user = document.createElement("p");
+  const userName = document.createElement("span");
+  userName.innerText = task.user.name;
+  const userIcon = document.createElement("span");
+  userIcon.innerText = task.user.icon;
+  user.appendChild(userName);
+  user.appendChild(userIcon);
+  thirdColumn.appendChild(user);
+  row.appendChild(thirdColumn);
 
-tableBody.appendChild(row);
+  tableBody.appendChild(row);
+};
+
+const initializeTable = nbTasks => {
+    for (let i = 0; i < nbTasks; i ++) {
+        const task = generateRandomTodos()[0];
+        addTaskToTable(task);
+    }
+}
+
+initializeTable(10);
 
 console.log(task);
---
+++ readme.md
@@ -0,0 +1,10 @@
+- Mettre en place une architecture modulaire ES6
+- Mettre en place un fichier service.mjs contenant un CRUD sur un tableau de tâches
+- Récupérer les tâches grâce au service et les afficher
+- Une fois les tâches affichées, implémenter la suppression d'une tâche
+- Lorsque je coche une case, implémenter ce changement d'état sur le serveur
+- Implémenter la création d'une tâche via une deuxième page
+- Mettre en place json-server (trouvable sur GitHub)
+- Dans le service, utiliser désormais l'API Fetch afin de ne plus utiliser le tableau
+- Adapter le code pour que les modifications soient effectives côté serveur
+- BONUS : afficher le détail d'une tâche sur une nouvelle page lors du clic sur celle-ci
